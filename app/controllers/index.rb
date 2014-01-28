21c6afee0db440cae97ab2d11caa47c5b0840903
Apprentice <apprentice@dbc-chicago-87.local>
2014-01-27 19:06:32 -0600

    work on display

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,28 +3,12 @@ get '/' do
   erb :index
 end
 
-get '/word' do
+post '/anagrams' do
   arr = Word.anagrams(params["user_input"])
-  string = "A list of anagrams for #{params["user_input"]}\n\n"
+  string = ""
   arr.each do |word|
     string << "#{word}\n\n"
   end
-  # redirect to("/?word=#{string}")
+  @word = string
+  erb :index
 end
-
-
-
-# get '/' do
-#   @grandma = params[:grandma]
-#   erb :index
-# end
-
-# post "/grandma" do
-#   input = params["user_input"]
-#   if input == input.upcase
-#     @grandma = "Oh, about 27 years ago."
-#   else
-#     @grandma = "SPEAK UP!"
-#   end
-#   redirect to("/?grandma=#{@grandma}"), 303
-# end
